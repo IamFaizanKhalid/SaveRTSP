3592b53c212a764effe4a58124577d335b9c7392
Faizan Khalid <IamFaizanKhalid@gmail.com>
2022-12-10 14:59:19 +0500

    Handle No Splitting

--- save.go
+++ save.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 )
 
 type Config struct {
@@ -71,31 +72,49 @@ func saveStream(outDir string, stream Stream) error {
 		return fmt.Errorf("unable to create the output directory: %w", err)
 	}
 
+	videoLength := "no limit"
+	if stream.Split > 0 {
+		videoLength = fmt.Sprintf(`%d minutes`, stream.Split)
+	}
+
 	fmt.Printf(`
 %s
 --------
 Input Stream: %s
 Output Directory: %s
-Video Length: %d minutes
+Video Length: %s
 
 `,
-		stream.Name, stream.Url, outDir, stream.Split,
+		stream.Name, stream.Url, outDir, videoLength,
+	)
+
+	opts := &ffmpeg.Options{
+		RTSPTransport:        pointer.String("tcp"),
+		NativeFramerateInput: pointer.Bool(true),
+		VideoCodec:           pointer.String("h264"),
+		AudioCodec:           pointer.String("aac"),
+		MapStreamId:          pointer.Int(0),
+	}
+
+	outputTime := time.Now().Format("20060102-1504")
+
+	if stream.Split > 0 {
+		opts.Format = pointer.String("segment")
+		opts.SegmentAtClockTime = pointer.Int(1)
+		opts.SegmentTime = pointer.Int(stream.Split * 60)
+		opts.SegmentFormat = pointer.String("mp4")
+		opts.SegmentNameByTime = pointer.Int(1)
+
+		outputTime = "%Y%m%d-%H%M"
+	}
+
+	outputFormat := fmt.Sprintf("%s/%s-%s.mp4",
+		outDir, strings.ReplaceAll(stream.Name, " ", "+"), outputTime,
 	)
 
 	return ffmpeg.New().
 		Input(stream.Url).
-		Output(fmt.Sprintf("%s/%s-%%Y%%m%%d-%%H%%M.mp4", outDir, strings.ReplaceAll(stream.Name, " ", "+"))).
-		Options(&ffmpeg.Options{
-			RTSPTransport:        pointer.String("tcp"),
-			NativeFramerateInput: pointer.Bool(true),
-			VideoCodec:           pointer.String("h264"),
-			AudioCodec:           pointer.String("aac"),
-			MapStreamId:          pointer.Int(0),
-			Format:               pointer.String("segment"),
-			SegmentAtClockTime:   pointer.Int(1),
-			SegmentTime:          pointer.Int(stream.Split * 60),
-			SegmentFormat:        pointer.String("mp4"),
-			SegmentNameByTime:    pointer.Int(1),
-		}).
+		Output(outputFormat).
+		Options(opts).
 		Start()
 }
