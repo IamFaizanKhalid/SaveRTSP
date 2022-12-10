ff86a67edbd3dbd700369cdce26891f6ff01105a
Faizan Khalid <IamFaizanKhalid@gmail.com>
2022-12-10 12:39:06 +0500

    Added ffmpeg Package

---
+++ ffmpeg/ffmpeg.go
@@ -0,0 +1,55 @@
+package ffmpeg
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func New() *ffmpeg {
+	return &ffmpeg{}
+}
+
+type ffmpeg struct {
+	input   *string
+	output  *string
+	options *Options
+}
+
+func (f *ffmpeg) Input(inputPath string) *ffmpeg {
+	f.input = &inputPath
+	return f
+}
+
+func (f *ffmpeg) Output(outputPath string) *ffmpeg {
+	f.output = &outputPath
+	return f
+}
+
+func (f *ffmpeg) Options(options Options) *ffmpeg {
+	f.options = &options
+	return f
+}
+
+func (f *ffmpeg) Start() error {
+	if f.input == nil {
+		return fmt.Errorf("no input provided")
+	}
+	inputArg := fmt.Sprintf("-i %s", *f.input)
+
+	outputArg := "output.mp4"
+	if f.output != nil {
+		outputArg = *f.output
+	}
+
+	if f.options == nil {
+		f.options = DefaultOptions()
+	}
+
+	args := []string{inputArg}
+	args = append(args, f.options.GetStrArguments()...)
+	args = append(args, outputArg)
+
+	cmd := exec.Command("ffmpeg", args...)
+
+	return cmd.Run()
+}
---
+++ ffmpeg/options.go
@@ -0,0 +1,80 @@
+package ffmpeg
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Options defines allowed FFmpeg arguments
+type Options struct {
+	NativeFramerateInput *bool   `flag:"-re"`                  // read input at native frame rate
+	Format               *string `flag:"-f"`                   // force format
+	VideoCodec           *string `flag:"-vcodec"`              // force video codec ('copy' to copy stream)
+	AudioCodec           *string `flag:"-acodec"`              // force audio codec ('copy' to copy stream)
+	RTSPTransport        *string `flag:"-rtsp_transport"`      // protocol to use to capture input streams
+	MapStreamId          *int    `flag:"-map"`                 // set stream mapping from input streams to output streams. Just enumerate the input streams in the order you want them in the output
+	SegmentAtClockTime   *int    `flag:"-segment_atclocktime"` // if set to "1" split at regular clock time intervals starting from 00:00 o’clock
+	SegmentTime          *string `flag:"-segment_time"`        // set segment duration (in seconds)
+	SegmentFormat        *string `flag:"-segment_format"`      // force format for the segments
+	SegmentNameByTime    *int    `flag:"-strftime"`            // if set to "1" segments will be named by time of file creation
+}
+
+func DefaultOptions() *Options {
+	t := true
+	transport := "tcp"
+	copyCodec := "copy"
+	mp4 := "mp4"
+
+	return &Options{
+		NativeFramerateInput: &t,
+		Format:               &mp4,
+		VideoCodec:           &copyCodec,
+		AudioCodec:           &copyCodec,
+		RTSPTransport:        &transport,
+	}
+}
+
+// GetStrArguments ...
+func (opts Options) GetStrArguments() []string {
+	f := reflect.TypeOf(opts)
+	v := reflect.ValueOf(opts)
+
+	values := []string{}
+
+	for i := 0; i < f.NumField(); i++ {
+		flag := f.Field(i).Tag.Get("flag")
+		value := v.Field(i).Interface()
+
+		if !v.Field(i).IsNil() {
+
+			if _, ok := value.(*bool); ok {
+				values = append(values, flag)
+			}
+
+			if vs, ok := value.(*string); ok {
+				values = append(values, flag, *vs)
+			}
+
+			if va, ok := value.([]string); ok {
+
+				for i := 0; i < len(va); i++ {
+					item := va[i]
+					values = append(values, flag, item)
+				}
+			}
+
+			if vm, ok := value.(map[string]string); ok {
+				for k, v := range vm {
+					values = append(values, flag, fmt.Sprintf("%v:%v", k, v))
+				}
+			}
+
+			if vi, ok := value.(*int); ok {
+				values = append(values, flag, fmt.Sprintf("%d", *vi))
+			}
+
+		}
+	}
+
+	return values
+}
